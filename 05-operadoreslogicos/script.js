45688e0f91e77b6c3ae26219fbc35ec6978e62d6
NICOLLE AILEEN ARAUJO DE ANDRADE <nicolle.aaandrade@senacsp.edu.br>
2026-02-26 11:56:18 -0300

    26/02

---
+++ 05-operadoreslogicos/script.js
@@ -0,0 +1,12 @@
+//operadores lógicos
+const condicao1 = true;
+const condicao2 = false;
+console.log(condicao1 && condicao2); //And
+console.log(condicao1 || condicao2); //Or
+
+const condicao3 = true;
+const condicao4 = !false; //Not
+console.log(condicao3 && condicao4); //And
+
+const condicao5 = 4;
+const condicao6 = 4;
