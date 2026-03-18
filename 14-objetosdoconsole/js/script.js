0327292de7a5816ad8c10bafd4923e849f341f67
NICOLLE AILEEN ARAUJO DE ANDRADE <nicolle.aaandrade@senacsp.edu.br>
2026-03-18 11:36:14 -0300

    modulos

---
+++ 14-objetosdoconsole/js/script.js
@@ -0,0 +1,14 @@
+//Exibindo uma mensagem de aviso no console
+console.warn("Atenção: esta é uma mensagem de aviso.");
+ 
+//Exibindo uma mensagem de erro no console
+console.error("Ocorreu um erro.");
+ 
+//Exibindo uma mensagem informativa no console
+console.info("Esta é uma mensagem informativa.");
+ 
+//Criando um grupo de mensagens no console
+console.group("Mensagens de teste");
+console.log("Esta é uma mensagem de teste.");
+console.log("Esta é outra mensagem de teste.");
+console.groupEnd();
